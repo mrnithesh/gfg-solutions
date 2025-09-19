f3205bb8ea5a94d4b46699f8d3f3b201b16b83d8
mrnithesh <mr.nithesh.k@gmail.com>
2025-09-19 19:15:42 +0530

    Add solution to check if a string is a palindrome - java

---
+++ Java/Palindrome String.java
@@ -0,0 +1,15 @@
+class Solution {
+    boolean isPalindrome(String s) {
+        // code here
+        int l = 0, r = s.length()-1;
+        while (l<r){
+            if (s.charAt(l) != s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+        
+    }
+}
