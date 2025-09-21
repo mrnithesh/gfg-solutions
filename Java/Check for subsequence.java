cb6276a6020588f8e846c425db2ab64a78d647ed
mrnithesh <mr.nithesh.k@gmail.com>
2025-09-21 18:31:27 +0530

    Add implementation to check for subsequence in two strings - java

---
+++ Java/Check for subsequence.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+
+class Solution {
+    boolean isSubSequence(String A, String B) {
+        int i=0;
+        int j=0;
+        while (i<A.length() && j<B.length()){
+            if (A.charAt(i) == B.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        return i==A.length();
+    }
+}
