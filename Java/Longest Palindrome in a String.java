450fd4c7c7c156f053754fc0ced9ffd97aef4f5e
mrnithesh <mr.nithesh.k@gmail.com>
2025-09-21 13:34:38 +0530

    Add implementation to find the longest palindrome in a string - java

---
+++ Java/Longest Palindrome in a String.java
@@ -0,0 +1,33 @@
+
+class Solution {
+    static String longestPalindrome(String s) {
+        // code here
+        String res="";
+        int resLen = 0;
+        for (int i=0;i<s.length();i++){
+            int l=i,r=i;
+            while (l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+                int currLen = r-l+1;
+                if (currLen>resLen){
+                    res = s.substring(l,r+1);
+                    resLen = currLen;
+                }
+                r++;
+                l--;
+            }
+            l=i;
+            r=i+1;
+            while (l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+                int currLen = r-l+1;
+                if (currLen>resLen){
+                    res = s.substring(l,r+1);
+                    resLen = currLen;
+                }
+                r++;
+                l--;
+            }
+        }
+        return res;
+        
+    }
+}
