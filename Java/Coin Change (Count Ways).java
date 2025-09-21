56e273f37f16951d103e8cfc10559e964077698a
mrnithesh <mr.nithesh.k@gmail.com>
2025-09-21 20:34:15 +0530

    Add implementation for counting ways to make change using dynamic programming - java

---
+++ Java/Coin Change (Count Ways).java
@@ -0,0 +1,12 @@
+class Solution {
+    public int count(int coins[], int sum) {
+        int [] dp = new int[sum+1];
+        dp[0] =1;
+        for(int coin: coins){
+            for(int i=coin;i<=sum;i++){
+                dp[i]+=dp[i-coin];
+            }
+        }
+        return dp[sum];
+    }
+}
